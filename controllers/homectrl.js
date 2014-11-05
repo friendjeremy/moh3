dd076c6ae45eed3612cc75fb22db130ea4f156b0
Hubert,Wandrille (wh1572) <wh1572@att.com>
2014-11-05 15:01:10 -0600

    home page stuff
    added stuff

--- controllers/homectrl.js
+++ controllers/homectrl.js
@@ -4,5 +4,12 @@ app.controller("HomeController",  function($scope){
         firstName: "home",
         lastName: "page"
     };
+
+    $scope.BackgroundImage = {
+background: 'url(2008_dallas_skyline.jpg)',
+'background-size': 'cover',
+'background-repeat': 'no-repeat',
+'background-position': 'center center'
+};
 });
  
