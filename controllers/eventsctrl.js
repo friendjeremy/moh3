8c7d87780967da270303306eee23253b0ac79a40
Hubert,Wandrille (wh1572) <wh1572@att.com>
2015-01-10 19:56:48 -0600

    Revert "modal code added to events"
    This reverts commit b168ad63eaaacb0b3d9d77f21e7b7cb6002abd81.

--- controllers/eventsctrl.js
+++ controllers/eventsctrl.js
@@ -1,4 +1,4 @@
-app.controller("EventsController", function ($scope, $modal, $log){
+app.controller("EventsController",  function($scope){
 
 	$scope.BackgroundImage = {
     	background: 'url(UC4LifeFlyer.png)',
@@ -7,43 +7,6 @@ app.controller("EventsController", function ($scope, $modal, $log){
         'background-position': 'center center'
 	};
 
-	$scope.items = ['item1', 'item2', 'item3'];
-
-	  $scope.open = function (size) {
-
-	    var modalInstance = $modal.open({
-	      templateUrl: 'myModalContent.html',
-	      controller: 'ModalInstanceCtrl',
-	      size: size,
-	      resolve: {
-	        items: function () {
-	          return $scope.items;
-	        }
-	      }
-	    });
-
-	    modalInstance.result.then(function (selectedItem) {
-	      $scope.selected = selectedItem;
-	    }, function () {
-	      $log.info('Modal dismissed at: ' + new Date());
-	    });
-	  };
-});
-
-app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
 });
 
 
