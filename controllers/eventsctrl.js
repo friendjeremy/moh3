b168ad63eaaacb0b3d9d77f21e7b7cb6002abd81
buggady <nicolasdeshefy@gmail.com>
2015-01-10 19:35:38 -0600

    modal code added to events
    half way working modal

--- controllers/eventsctrl.js
+++ controllers/eventsctrl.js
@@ -1,4 +1,4 @@
-app.controller("EventsController",  function($scope){
+app.controller("EventsController", function ($scope, $modal, $log){
 
 	$scope.BackgroundImage = {
     	background: 'url(UC4LifeFlyer.png)',
@@ -7,6 +7,43 @@ app.controller("EventsController",  function($scope){
         'background-position': 'center center'
 	};
 
+	$scope.items = ['item1', 'item2', 'item3'];
+
+	  $scope.open = function (size) {
+
+	    var modalInstance = $modal.open({
+	      templateUrl: 'myModalContent.html',
+	      controller: 'ModalInstanceCtrl',
+	      size: size,
+	      resolve: {
+	        items: function () {
+	          return $scope.items;
+	        }
+	      }
+	    });
+
+	    modalInstance.result.then(function (selectedItem) {
+	      $scope.selected = selectedItem;
+	    }, function () {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });
+	  };
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
 
 
